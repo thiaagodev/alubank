e047d717fcba46b30fbb4f7dabcf26c359da53b1
Thiago Ferreira <thiaago.dev@gmail.com>
2023-03-28 14:44:23 -0300

    criando widget content division

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:alubank/components/box_card.dart';
 import 'package:alubank/components/color_dot.dart';
+import 'package:alubank/components/content_division.dart';
 import 'package:alubank/components/sections/header.dart';
 import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
@@ -14,9 +15,7 @@ class Home extends StatelessWidget {
         children: [
           const Header(),
           BoxCard(
-            boxContent: ColorDot(
-              color: ThemeColors.recentActivity['income'],
-            ),
+            boxContent: ContentDivision(),
           ),
         ],
       ),
